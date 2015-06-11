18bb3b98613e62ceb141003fc881ac22ce8750bb
Elliott Draper <el@kickcode.com>
2015-06-11 16:06:05 +0100

    error pointer example during file saving

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,6 +2,16 @@ class AppDelegate
   def applicationDidFinishLaunching(notification)
     buildMenu
     buildWindow
+
+    error_pointer = Pointer.new(:object)
+    result = "this is some test text".dataUsingEncoding(NSUTF8StringEncoding).writeToFile("/Users/#{NSUserName()}/test.txt", options: NSDataWritingWithoutOverwriting, error: error_pointer)
+    
+    if result
+      NSLog "File saved successfully."
+    else
+      error = error_pointer[0]
+      NSLog "Unable to save file - #{error.localizedDescription}"
+    end
   end
 
   def buildWindow

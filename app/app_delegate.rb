5df169322c815b46e7c9320f216ce67f3e9dcd92
Elliott Draper <el@kickcode.com>
2015-06-11 16:12:34 +0100

    contrived example of a dangling pointer left behind

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,13 +3,17 @@ class AppDelegate
     buildMenu
     buildWindow
 
-    error_pointer = Pointer.new(:object)
-    result = "this is some test text".dataUsingEncoding(NSUTF8StringEncoding).writeToFile("/Users/#{NSUserName()}/test.txt", options: NSDataWritingWithoutOverwriting, error: error_pointer)
-    
-    if result
+    @error_pointer = Pointer.new(:object)
+    @result = "this is some test text".dataUsingEncoding(NSUTF8StringEncoding).writeToFile("/Users/#{NSUserName()}/test.txt", options: NSDataWritingWithoutOverwriting, error: @error_pointer)
+
+    NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: 'log_result', userInfo: nil, repeats: false)
+  end
+
+  def log_result
+    if @result
       NSLog "File saved successfully."
     else
-      error = error_pointer[0]
+      error = @error_pointer[0]
       NSLog "Unable to save file - #{error.localizedDescription}"
     end
   end
